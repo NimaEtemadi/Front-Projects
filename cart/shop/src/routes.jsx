fb32b65938482164d3ab35022599b7475367de47
NimaEtemadi <Nima.etemadi1989@gmail.com>
2023-07-24 20:20:55 +0330

    :deleteCretProject

--- cart/shop/src/routes.jsx
+++
@@ -1,9 +0,0 @@
-import Index from "./Pages/Index/Index"; 
-import Cart from "./Pages/Cart/Cart";
-const routes = [
-  { path: "/", element: <Index /> },
-  { path: "/cart", element: <Cart /> },
- 
-];
-
-export default routes;
