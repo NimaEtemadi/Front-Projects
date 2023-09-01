9012b4a0df1b9b5279ceb71dcbde0b81486173ca
NimaEtemadi <Nima.etemadi1989@gmail.com>
2023-09-01 23:22:36 +0330

    deleteBookProject

--- Book-Landing-Page/JavaScript/app.js
+++
@@ -1,9 +0,0 @@
-// tooltips
-function inital_bootstrap_tooltips(){
-    const tooltips = document.querySelectorAll(".tt")
-    tooltips.forEach((t)=>{
-        new bootstrap.Tooltip(t)
-    })
-}
-
-inital_bootstrap_tooltips()
