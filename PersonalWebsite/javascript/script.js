155438d621e49a5e717998bae4e7ed4c7ac08b1b
NimaEtemadi <Nima.etemadi1989@gmail.com>
2023-09-22 14:40:51 +0330

    PersonalWebsiteJs

---
+++ PersonalWebsite/javascript/script.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".navbarMobile");
+const mobileMenu = document.querySelector(".mobileMenu");
+const closeBtn = document.querySelector(".CloseBtn");
+
+//method of open mobile menu
+const mobileNavbarOpen = () =>{
+    menuBtn.style.display = "none";
+    mobileMenu.style.display="block";
+}
+
+//method of close mobile menu
+const mobileNavbarClose = () =>{
+    menuBtn.style.display = "block";
+    mobileMenu.style.display="none";
+}
+
+menuBtn.addEventListener("click",mobileNavbarOpen);
+closeBtn.addEventListener("click",mobileNavbarClose);
